0036662bceb6433b154cba50a4f2a60e073c6f0b
Onyebuci Matthew Mordi <mordio30@gmail.com>
2025-03-02 09:04:28 -0800

    password issues

--- app.py
+++ app.py
@@ -9,12 +9,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class Students(db.Model):  # referencing our table as a Model
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    age = db.Column(db.Integer)
+    grade = db.Column(db.String(2))
+
+
 @app.route('/old_students/', methods=['GET'])
 def old_students():
     query = text("SELECT * FROM students WHERE age >= 21")
     result = db.session.execute(query)
     students = [dict(row) for row in result.mappings()]
     return jsonify(students)
+
 
 @app.route('/advance_students/', methods=['GET'])
 def advance_students():
@@ -23,12 +32,14 @@ def advance_students():
     students = [dict(row) for row in result.mappings()]
     return jsonify(students)
 
+
 @app.route('/student_names/', methods=['GET'])
 def student_names():
     query = text("SELECT first_name, last_name FROM students")
     result = db.session.execute(query)
     students = [dict(row) for row in result.mappings()]
     return jsonify(students)
+
 
 @app.route('/student_ages/', methods=['GET'])
 def student_ages():
@@ -37,6 +48,7 @@ def student_ages():
     students = [dict(row) for row in result.mappings()]
     return jsonify(students)
 
+
 @app.route('/students/', methods=['GET'])
 def all_students():
     query = text("SELECT * FROM students")
@@ -44,5 +56,6 @@ def all_students():
     students = [dict(row) for row in result.mappings()]
     return jsonify(students)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+
+# if __name__ == '__main__':
+app.run(debug=True, port=8000)
